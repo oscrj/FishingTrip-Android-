95ed32dcaafaca0c451f420fb936a09cbb162b55
oscrj <oscar.johanneson@gmail.com>
2020-06-05 14:44:56 +0200

    Created Login, Register and Profile Activities.

--- app/src/main/java/com/example/fishingtrip/MainActivity.java
+++ app/src/main/java/com/example/fishingtrip/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.fishingtrip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +21,50 @@ public class MainActivity extends AppCompatActivity {
         btnRegister = findViewById(R.id.btn_register);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        /**
+         *  On click user start Login Activity
+         */
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginActivity = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginActivity);
+            }
+        });
+
+        /**
+         *  On click user start Register Activity
+         */
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerActivity = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerActivity);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
