712e14694b69d8eb2c40d7d6fdd8d8c19ffad954
oscrj <oscar.johanneson@gmail.com>
2020-06-05 12:46:40 +0200

    Set color scheme.

--- app/src/main/java/com/example/fishingtrip/MainActivity.java
+++ app/src/main/java/com/example/fishingtrip/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.fishingtrip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_register);
     }
+
+
 }
